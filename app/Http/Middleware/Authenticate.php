b5e063cd36a79d2ba8d53ea9c817d98c97f703f3
Mochamad akbar <mochakbar912@gmail.com>
2022-12-01 12:11:05 +0700

    feat(middleware): create middleware auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,8 +1,10 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
 
 class Authenticate extends Middleware
 {
@@ -10,12 +12,20 @@ class Authenticate extends Middleware
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @return string|null|JsonResponse
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (
+            $request->is('api/v1/*') &&
+            !Auth::guard(config('auth.guards.api.name'))->check()
+        ) {
+            return response()->json([
+                'code' => Response::HTTP_UNAUTHORIZED,
+                'status' => Response::$statusTexts[Response::HTTP_UNAUTHORIZED]
+            ], Response::HTTP_UNAUTHORIZED);
         }
+
+        return route('welcome');
     }
 }
